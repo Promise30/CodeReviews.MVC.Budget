c031f80b061ec84abfeef0c98f013509c4277ff6
Promise30 <promiseadeagbo@gmail.com>
2025-05-30 18:39:38 +0100

    Added property for start date and end date

--- Budget-MVC/Models/FilterParameters.cs
+++ Budget-MVC/Models/FilterParameters.cs
@@ -3,6 +3,7 @@
     public class FilterParameters
     {
         public string? Category { get; set; } = string.Empty;
-        public DateTime? Date { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
     }
 }
